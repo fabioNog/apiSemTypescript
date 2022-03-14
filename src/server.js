20977e6436d035cb6987dc38d554df08d3bc3eeb
fabio.nogueira.carmo <fabio.nogueira@soitic.com>
2022-03-14 17:04:23 -0300

    alter file server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,4 @@
-const express = require('express');
-
-const app = express();
-
-app.get('/', (req,res) => {
-    return res.status(200).json({
-        status: 'ok',
-        messagem: 'requisição feita com o get'
-    })
-})
+const app = require('./app')
 
 
-app.listen(3000,function(){
-    console.log('Servidor funcionando');
-})
+app.listen(process.env.PORT || 3000);
