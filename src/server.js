e7cb4d3a4f154a2f7506b29cd48c30208747360a
fabio.nogueira.carmo <fabio.nogueira@soitic.com>
2022-03-14 16:48:30 -0300

    Create function get

--- src/server.js
+++ src/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req,res) => {
+    return res.status(200).json({
+        status: 'ok',
+        messagem: 'requisição feita com o get'
+    })
+})
+
 
 app.listen(3000,function(){
     console.log('Servidor funcionando');
